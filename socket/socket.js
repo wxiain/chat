13e3994c4d882a25cf5d1b6e414f704085b6460d
dlh <qlhdtly12189@163.com>
2019-12-12 08:59:19 +0800

    可以给特定用户发消息

--- socket/socket.js
+++ socket/socket.js
@@ -1,26 +1,25 @@
 const socket = require('socket.io');
 let arrUser = [];
-
+let i = 0;
 module.exports = serve => {
     let io = socket.listen(serve);
     // 连接
     io.sockets.on('connection', (socket) => {
+        let id = socket.id;
         let query = socket.handshake.query;
         // console.log(query);
         socket.on('userLink', (data) => {
-            arrUser.push(data);
+            arrUser.push({
+                id,
+                ...data
+            });
             console.log(arrUser);
         });
         socket.on('call', data => {
             let bothId = data.bothId;
-            let id = data.user;
-            console.log(data);
-            console.log(id, '发送的消息', arrUser.findIndex(i => i.id === bothId) > -1);
-            if (arrUser.findIndex(i => i.userId === bothId) > -1) {
-                // let toSocket = _.findWhere(io.sockets.sockets, {id: bothId});
-                console.log(io.sockets.sockets);
-                /*toSocket.emit('message', data.content);
-                console.log('执行完毕');*/
+            let index = arrUser.findIndex(i => i.userId === bothId);
+            if (index > -1) {
+                socket.to(arrUser[index].id).emit('message', data.content);
             }
         })
         // socket.emit('receive');
@@ -28,8 +27,8 @@ module.exports = serve => {
         // io.broadcast.emit('new_user', {msg: '消息推送'});
         // https://www.jianshu.com/p/07a167b1482b
     });
-    // 消息发送
-    io.sockets.on('message', socket => {
-        socket.on('')
-    })
+    // // 消息发送
+    // io.sockets.on('message', socket => {
+    //     socket.on('')
+    // })
 };
